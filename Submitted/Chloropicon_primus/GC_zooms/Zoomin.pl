9e491176bc2065face30d14d82c9ea674cce83d5
Jean-Francois Pombert <jpombert@iit.edu>
2019-05-15 11:13:59 -0500

    Better docs

--- Submitted/Chloropicon_primus/GC_zooms/Zoomin.pl
+++ Submitted/Chloropicon_primus/GC_zooms/Zoomin.pl
@@ -1,21 +1,46 @@
 #!/usr/bin/perl
-## Pombert Lab, 2017
-## v0.1
+## Pombert Lab, 2019
+my $name = 'Zoomin.pl';
+my $version = 0.2;
 
-use strict;
-use warnings;
+use strict; use warnings; use Getopt::Long qw(GetOptions);
 
-die "\nUSAGE = Zoomin.pl Verified_products_ALL.list start end chromosome_XX.embl chromosome_XX.fsa\n\n" unless @ARGV;
+## Defining options
+my $options = <<"OPTIONS";
 
-my $products = $ARGV[0];
-my $start = $ARGV[1];
-my $end = $ARGV[2];
-my $chromo = $ARGV[3];
-my $fasta = $ARGV[4];
+NAME		$name
+VERSION		$version
+SYNOPSIS	Calculates the GC content of a locus and returns the genes contained within the locus
+USAGE		Zoomin.pl -f chromosome_XX.fsa -s start -e end -l Verified_products_ALL.list -embl chromosome_XX.embl 
+
+EXAMPLE		Zoomin.pl -f Chromosome_03.fsa -s 1337500 -e 1369999 -l Verified_products_ALL.list -embl Chromosome_03.embl 
+
+OPTIONS:
+-f (--fasta)	FASTA file of the chromosome or contig
+-s (--start)	Start of the region to look at
+-e (--end)	End of the region to look at
+-l (--list)	List of locus_tags and their products
+-embl		Annotations in EMBL format
+OPTIONS
+die "$options\n" unless @ARGV;
+
+my $products;
+my $start;
+my $end;
+my $fasta;
+my $embl;
+
+GetOptions(
+	'l|list=s' => \$products,
+	's|start=i' => \$start,
+	'e|end=i' => \$end,
+	'f|fasta=s' => \$fasta,
+	'embl=s' => \$embl
+);
 
 ## Creating database of products
 my %products;
-open PROD, "<$products";
+open PROD, "<$products" or die "Can\'t open products file $products...\n";
 while (my $line = <PROD>){
 	chomp $line;
 	if ($line =~ /^#/){next;}
@@ -26,7 +51,7 @@ while (my $line = <PROD>){
 }
 
 ## Calculating GC content in the zoomed in region
-open FASTA, "<$fasta";
+open FASTA, "<$fasta" or die "Can\'t open fasta file $fasta...\n";
 my $sequence;
 while (my $line = <FASTA>){
 	chomp $line;
@@ -44,10 +69,10 @@ my $percent = sprintf("%.1f", ($GC/$size)*100);
 my $gene_start; my $gene_end;
 my $locus; my $key;
 my $strand;
-open EMBL, "<$chromo";
-$chromo =~ s/.embl$//;
-open OUT, ">$chromo.zoomin";
-print OUT "## Zoom in on $chromo from position $start to position $end; Length $size, GC% $percent\n";
+open EMBL, "<$embl" or die "Can\'t open EMBL file $embl...\n";
+$embl =~ s/.embl$//;
+open OUT, ">$embl.zoomin";
+print OUT "## Zoom in on $embl from position $start to position $end; Length $size, GC% $percent\n";
 print OUT "Locus tag\tstrand\tStart\tEnd\tProduct\n";
 while (my $line = <EMBL>){
 	chomp $line;
--- Submitted/Chloropicon_primus/GC_zooms/low_gc.pl
+++ Submitted/Chloropicon_primus/GC_zooms/low_gc.pl
@@ -1,20 +1,41 @@
 #!/usr/bin/perl
+## Pombert Lab, 2019
+my $name = 'low_gc.pl';
+my $version = 0.2;
 
-use strict;
-use warnings;
+use strict; use warnings; use Getopt::Long qw(GetOptions);
 
-die "\nUSAGE = low_gc.pl maxGC genome.gc\n\n" unless @ARGV;
+## Defining options
+my $options = <<"OPTIONS";
 
-my $gc = $ARGV[0];
-my $file = $ARGV[1];
+NAME		$name
+VERSION		$version
+SYNOPSIS	Returns genomic regions with GC content lower/higher than specifed value
+USAGE		low_gc.pl -max 50 -gc genome.gc
+
+OPTIONS:
+-max		GC cutoff <= value [Default: 50]
+-gc		GC content file (generated with GC_content_to_Circos.pl) [Default: genome.gc]
+OPTIONS
+die "$options\n" unless @ARGV;
+
+my $gc = 50;
+my $file = 'genome.gc';
+GetOptions(
+	'max=i' => \$gc,
+	'gc=s' => \$file
+);
+
 
 open IN, "<$file";
-open OUT, ">$file.$gc.low_gc";
+open LOW, ">$file.$gc.low_gc"; ## <= specified GC cutoff
+open HIGH, ">$file.$gc.high_gc"; ## > specified GC cutoff
 while (my $line = <IN>){
 	chomp $line;
 	if ($line =~ /^#/){next;}
 	else{
 		my @cols = split(" ", $line);
-		if ($cols[3] <= $gc){print OUT "$line\n";}
+		if ($cols[3] <= $gc){print LOW "$line\n";}
+		else {print HIGH "$line\n";}
 	}
 }

2124691b217ce69c6245892e62b3c17111c4552c
Jean-Francois Pombert <jpombert@iit.edu>
2019-05-15 12:14:46 -0500

    Better docs

--- Submitted/Chloropicon_primus/BLAST/Uniprot/hash_products.pl
+++ Submitted/Chloropicon_primus/BLAST/Uniprot/hash_products.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/perl
-## Get products list from trEMBL and SwissProt fasta headers
+## Pombert Lab, 2017
+my $version = '0.1';
+my $name = 'hash_products.pl';
 
-use strict;
-use warnings;
+use strict; use warnings;
+
+## Defining options
+my $options = <<"OPTIONS";
+
+NAME		$name
+VERSION		$version
+SYNOPSIS	Generates lists of products from trEMBL and SwissProt fasta headers
+USAGE		hash_products.pl *.fasta
+OPTIONS
+die "$options\n" unless @ARGV;
 
 while (my $file = shift@ARGV){
 	open IN, "<$file";
--- Submitted/Chloropicon_primus/BLAST/Uniprot/parse_UniProt_BLASTs.pl
+++ Submitted/Chloropicon_primus/BLAST/Uniprot/parse_UniProt_BLASTs.pl
@@ -1,22 +1,42 @@
 #!/usr/bin/perl
-## Pombert Lab, IIT, 2016
-## Requires BLAST tabular output (outfmt 6)
+## Pombert Lab, IIT, 2019
 ## Requires tab-separated accession number / product list 
+my $version = '0.2';
+my $name = 'parse_UniProt_BLASTs.pl';
 
-use strict;
-use warnings;
+use strict; use warnings; use Getopt::Long qw(GetOptions);
 
-my $usage = 'USAGE = perl parse_UniProt_BLASTs.pl query_list Uniprot_product_list blast_output';
-die "\n$usage\n
-query_list= list of proteins queried against UniProt
-Uniprot_product_list= tab-delimited UniProt accesssion number/product list
-blast_output = BLAST tabular output (outfmt 6)\n
-" unless (scalar@ARGV==3);
+## Defining options
+my $options = <<"OPTIONS";
 
-open QUERIES, "<$ARGV[0]";
-open PRODUCTS, "<$ARGV[1]";
-open BLAST, "<$ARGV[2]";
-open OUT, ">$ARGV[2].parsed";
+NAME		$name
+VERSION		$version
+SYNOPSIS	Generates a tab-delimited list of products found/not found with BLAST searches
+USAGE		parse_UniProt_BLASTs.pl -b blast_output -q query.list -u uniprot_list -o parsed.tsv
+
+OPTIONS:
+-b (--blast)	BLAST tabular output (outfmt 6)
+-q (--query)	List of proteins queried against UniProt
+-u (--uniprot)	Tab-delimited list of UniProt accesssion numbers/products ## Created with hash_products.pl
+-o (--output)	Desired output name
+OPTIONS
+die "$options\n" unless @ARGV;
+
+my $blast;
+my $query;
+my $uniprot;
+my $output;
+GetOptions(
+	'b|blast=s' => \$blast,
+	'q|query=s' => \$query,
+	'u|uniprot=s' => \$uniprot,
+	'o|output=s' => \$output
+);
+
+open QUERIES, "<$query";
+open PRODUCTS, "<$uniprot";
+open BLAST, "$blast";
+open OUT, ">$output";
 
 my %products = (); ## Empty product hash; WARNING will take a decent chunk of RAM for 
 my %hits = (); ## Keeping track of BLAST hits
@@ -31,18 +51,17 @@ while (my $line = <PRODUCTS>){
 ## Parsing BLAST
 while (my $line = <BLAST>){
 	chomp $line;
-	if ($line =~ /^(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S+)/){
-		my $query = $1;
-		my $hit = $2;
-		my $evalue =$11;
-		if (exists $hits{$query}){next;}
-		elsif ($products{$hit} =~ /uncharacterized/i){next;} ## Discarding uninformative BLAST hists
-		elsif ($products{$hit} =~ /hypothetical/i){next;} ## Discarding uninformative BLAST hists
-		elsif ($products{$hit} =~ /predicted protein/i){next;} ## Discarding uninformative BLAST hists
-		else{
-			$hits{$query}=$products{$hit};
-			$evalues{$query}=$evalue;
-		}
+	my @columns = split("\t", $line);
+	my $query = $columns[0];
+	my $hit = $columns[1];
+	my $evalue =$columns[10];
+	if (exists $hits{$query}){next;}
+	elsif ($products{$hit} =~ /uncharacterized/i){next;} ## Discarding uninformative BLAST hists
+	elsif ($products{$hit} =~ /hypothetical/i){next;} ## Discarding uninformative BLAST hists
+	elsif ($products{$hit} =~ /predicted protein/i){next;} ## Discarding uninformative BLAST hists
+	else{
+		$hits{$query}=$products{$hit};
+		$evalues{$query}=$evalue;
 	}
 }
 ## Writing parsed list
@@ -51,11 +70,7 @@ while (my $line = <QUERIES>){
 	chomp $line;
 	if ($line =~ /^(\S+)/){
 		my $query = $1;
-		if (exists $hits{$query}){
-			print OUT "$query\t$evalues{$query}\t$hits{$query}\n";
-		}
-		else{
-			print OUT "$query\tN\/A\thypothetical protein\n";
-		}
+		if (exists $hits{$query}){print OUT "$query\t$evalues{$query}\t$hits{$query}\n";}
+		else{print OUT "$query\tN\/A\thypothetical protein\n";}
 	}
 }

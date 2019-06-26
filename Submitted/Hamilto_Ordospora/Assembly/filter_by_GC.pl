fb9483b23abde1dea492751ea7606080ee138362
Jean-Francois Pombert <jpombert@iit.edu>
2019-06-26 11:04:45 -0500

    Added docs
    Also reduced complexity by integrating sums to the scripts

--- Submitted/Hamilto_Ordospora/Assembly/filter_by_GC.pl
+++ Submitted/Hamilto_Ordospora/Assembly/filter_by_GC.pl
@@ -1,19 +1,43 @@
 #!/usr/bin/perl
+## Pombert Lab, 2017
+my $name = 'filter_by_GC.pl';
+my $version = 0.1;
 
-use strict;
-use warnings;
+use strict; use warnings; use Getopt::Long qw(GetOptions);
 
-my $usage = "USAGE = filterGC.pl minGC maxGC *.fasta";
-die "\n$usage\n\n" unless @ARGV;
+my $options = <<"OPTIONS";
 
-my $min = shift@ARGV;
-my $max = shift@ARGV;
+NAME		$name
+VERSION		$version
+SYNOPSIS	Parses contigs by their GC content
+USAGE		filter_by_GC.pl -min 20 -max 60 -fa *.fasta -out GC_summary.txt
 
-while (my $fasta = shift@ARGV){
+OPTIONS:
+-min		Desired minimum GC percentage [default: 20]
+-max		Desired maximum GC percentage [default: 60]
+-fa		Fasta file(s) to parse
+-out		GC content summary output [default: GC_summary.txt]
+OPTIONS
+die "$options\n" unless @ARGV;
+
+my $min = 20;
+my $max = 60;
+my @fasta;
+my $output;
+GetOptions(
+	'min=i' => \$min,
+	'max=i' => \$max,
+	'fa|fasta=s@{1,}' => \@fasta,
+	'out=s' => \$output
+);
+
+open OUT, ">$output";
+while (my $fasta = shift@fasta){
 	open IN, "<$fasta";
-	open MIN, ">$fasta.mingc";
-	open MED, ">$fasta.medgc";
-	open MAX, ">$fasta.maxgc";
+	$fasta =~ s/\.w+//;
+	open MIN, ">$fasta.${min}-.GC.fasta";
+	open MED, ">$fasta.${min}_to_${max}.GC.fasta";
+	open MAX, ">$fasta.${max}+.GC.fasta";
 	my %sequences;
 	my $contig;
 	my @contigs;
@@ -29,29 +53,44 @@ while (my $fasta = shift@ARGV){
 	}
 	while (my $cg = shift@contigs){
 		my $gc = 0;
-		my $lenght = length($sequences{$cg});
-		for (my $i = 0; $i <= $lenght - 1;  $i++){
+		my $length = length($sequences{$cg});
+		for (my $i = 0; $i <= $length - 1;  $i++){
 			my $nt = substr($sequences{$cg}, $i, 1);
 			if ($nt eq 'G' || $nt eq 'g' || $nt eq 'C' || $nt eq 'c'){$gc++;}
 		}
-		my $percent = ($gc/$lenght)*100;
+		my $percent = ($gc/$length)*100;
 		if ($percent < $min){
-			print "UNDER\t$cg\tlenght = $lenght\tGC percent = $percent\n";
+			print OUT "UNDER\t$cg\tlength = $length\tGC percent = $percent\n";
 			print MIN ">$cg\n";
 			my @seq = unpack("(A60)*", $sequences{$cg});
 			while (my $sq = shift@seq){print MIN "$sq\n";}
 		}
 		elsif ($percent > $max){
-			print "OVER\t$cg\tlenght = $lenght\tGC percent = $percent\n";
+			print OUT "OVER\t$cg\tlength = $length\tGC percent = $percent\n";
 			print MAX ">$cg\n";
 			my @seq = unpack("(A60)*", $sequences{$cg});
 			while (my $sq = shift@seq){print MAX "$sq\n";}
 		}
 		else{
-			print "BETWEEN\t$cg\tlenght = $lenght\tGC percent = $percent\n";
+			print OUT "BETWEEN\t$cg\tlength = $length\tGC percent = $percent\n";
 			print MED ">$cg\n";
 			my @seq = unpack("(A60)*", $sequences{$cg});
 			while (my $sq = shift@seq){print MED "$sq\n";}
 		}
 	}
 }
+close OUT;
+open IN, "<$output";
+my $under = 0; my $between = 0; my $over = 0;
+while (my $line = <IN>){
+	chomp $line;
+	if ($line =~ /^UNDER.*length = (\d+)/){$under+=$1;}
+	elsif ($line =~ /^BETWEEN.*length = (\d+)/){$between+=$1;}
+	elsif ($line =~ /^OVER.*length = (\d+)/){$over+=$1;}
+}
+
+my $sum = $under + $between + $over;
+print "\nUnder ${min}\% GC\t\t$under bp\n";
+print "In-between ${min}-${max}\% GC\t$between bp\n";
+print "Over ${max}\ %GC\t\t$over bp\n";
+print "Total\t\t\t$sum bp\n\n";
--- Submitted/Hamilto_Ordospora/Assembly/filter_by_coverage.pl
+++ Submitted/Hamilto_Ordospora/Assembly/filter_by_coverage.pl
@@ -1,22 +1,37 @@
 #!/usr/bin/perl
+## Pombert Lab, 2017
+my $name = 'filter_by_coverage.pl';
+my $version = 0.1;
 
-use strict;
-use warnings;
-use Getopt::Long qw(GetOptions);
+use strict; use warnings; use Getopt::Long qw(GetOptions);
 
-my $usage = "USAGE = filter_by_coverage.pl -min min -max max -fa *.fasta -cov *.coverage";
-die "\n$usage\n\n" unless @ARGV;
+my $options = <<"OPTIONS";
 
-my $min;
-my $max;
+NAME		$name
+VERSION		$version
+SYNOPSIS	Parses contigs by their average sequencing depth
+USAGE		filter_by_coverage.pl -min 20 -max 200 -fa *.fasta -cov *.coverage -out depth_summary.txt
+
+OPTIONS:
+-min		Desired minimum sequencing depth [default: 20]
+-max		Desired maximum sequencing depth [default: 200]
+-fa		Fasta file(s) to parse
+-cov		Coverage files obtained by read mapping with get_SNPs.pl
+-out		Depth summary output [default: depth_summary.txt]
+OPTIONS
+die "$options\n" unless @ARGV;
+
+my $min = 20;
+my $max = 200;
 my @fasta;
 my @cov;
-
+my $output = 'depth_summary.txt';
 GetOptions(
-	'min=s' => \$min,
-	'max=s' => \$max,
+	'min=i' => \$min,
+	'max=i' => \$max,
 	'fa|fasta=s@{1,}' => \@fasta,
-	'cov|coverage=s@{1,}' => \@cov
+	'cov|coverage=s@{1,}' => \@cov,
+	'out=s' => \$output
 );
 
 my %coverage;
@@ -34,11 +49,13 @@ while (my $coverage = shift@cov){
 	} 
 }
 
+open OUT, ">$output";
 while (my $fasta = shift@fasta){
 	open IN, "<$fasta";
-	open MIN, ">$fasta.mincov";
-	open MED, ">$fasta.medcov";
-	open MAX, ">$fasta.maxcov";
+	$fasta =~ s/\.w+//;
+	open MIN, ">$fasta.${min}-X.fasta";
+	open MED, ">$fasta.${min}_to_${max}X.fasta";
+	open MAX, ">$fasta.${max}+X.fasta";
 	my %sequences;
 	my $contig;
 	my @contigs;
@@ -55,23 +72,37 @@ while (my $fasta = shift@fasta){
 	while (my $cg = shift@contigs){
 		my $avcov = $coverage{$cg}[1]/$coverage{$cg}[0];
 		if ($avcov < $min){
-			print "UNDER\t$cg\tlenght = $coverage{$cg}[0]\tAverage sequencing depth = $avcov\n";
+			print OUT "UNDER\t$cg\tlength = $coverage{$cg}[0]\tAverage sequencing depth = $avcov\n";
 			print MIN ">$cg\n";
 			my @seq = unpack("(A60)*", $sequences{$cg});
 			while (my $sq = shift@seq){print MIN "$sq\n";}
 		}
 		elsif ($avcov > $max){
-			print "OVER\t$cg\tlenght = $coverage{$cg}[0]\tAverage sequencing depth = $avcov\n";
+			print OUT "OVER\t$cg\tlength = $coverage{$cg}[0]\tAverage sequencing depth = $avcov\n";
 			print MAX ">$cg\n";
 			my @seq = unpack("(A60)*", $sequences{$cg});
 			while (my $sq = shift@seq){print MAX "$sq\n";}
 		}
 		else{
-			print "BETWEEN\t$cg\tlenght = $coverage{$cg}[0]\tAverage sequencing depth = $avcov\n";
+			print OUT "BETWEEN\t$cg\tlength = $coverage{$cg}[0]\tAverage sequencing depth = $avcov\n";
 			print MED ">$cg\n";
 			my @seq = unpack("(A60)*", $sequences{$cg});
 			while (my $sq = shift@seq){print MED "$sq\n";}
 		}
 	}
 }
-	
+close OUT;
+open IN, "<$output";
+my $under = 0; my $between = 0; my $over = 0;
+while (my $line = <IN>){
+	chomp $line;
+	if ($line =~ /^UNDER.*length = (\d+)/){$under+=$1;}
+	elsif ($line =~ /^BETWEEN.*length = (\d+)/){$between+=$1;}
+	elsif ($line =~ /^OVER.*length = (\d+)/){$over+=$1;}
+}
+
+my $sum = $under + $between + $over;
+print "\nUnder ${min}X\t\t$under bp\n";
+print "In-between ${min}-${max}X\t$between bp\n";
+print "Over ${max}X\t\t$over bp\n";
+print "Total\t\t\t$sum bp\n\n";
